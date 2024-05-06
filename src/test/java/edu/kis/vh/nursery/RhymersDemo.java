869e66d52f2c42cd4a71759b2196beb63f7281e3
Szymon Gieraga <245809@edu.p.lodz.pl>
2024-05-06 12:57:49 +0200

    4.2.1 Poprawa nazw metod interfejsu Rhymesfactory.java

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -3,19 +3,21 @@ package edu.kis.vh.nursery;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
+import java.util.Random;
+
 class RhymersDemo {
 
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
         
-        defaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
+        DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
         
         for (int i = 1; i < 15; i++)
             for (int j = 0; j < 3; j++)
                 rhymers[j].countIn(i);
         
-        java.util.Random rn = new java.util.Random();
+        Random rn = new Random();
         for (int i = 1; i < 15; i++)
             rhymers[3].countIn(rn.nextInt(20));
         
